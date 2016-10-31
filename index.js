fe08b602c9fbbdc3836235147cb7b02dcec76338
mac <mac@win7-air.local>
2016-10-31 18:53:37 +0800

    remove useless options

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ program
 	.option('-o, --output <string>', 'output directory')
 	.option('-l, --launchReport', 'launch the report page after finish')
 	.option('-r, --reportSuiteAsScenarios', 'report suite as scenarios')
-	.option('-n, --name', 'report name')
 	.parse(process.argv);
 
 var options = {
@@ -21,7 +20,6 @@ var options = {
     jsonFile: program.jsonFile,
     jsonDir: program.jsonDir,
     output: program.output,
-    name: program.name,
     reportSuiteAsScenarios: program.launchReport || true,
     launchReport: program.reportSuiteAsScenarios || true
 };
